67298a0e174dd7f5a81a5f279f6767f5dc979fc9
Azim Amin <azimamin@Azims-Mac-mini.local>
2025-03-17 17:54:38 +0800

    initialize project

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Customers</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap.css">
+    <link rel="stylesheet" href="./assets/css/fontawesome.min.css">
+    @stack('styles')
+</head>
+
+<body>
+
+    @yield('content')
+
+    <script src="{{ asset('assets/js/jquery.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.js') }}"></script>
+    <script src="{{ asset('assets/js/fontawesome.js') }}"></script>
+
+</body>
+
+</html>
